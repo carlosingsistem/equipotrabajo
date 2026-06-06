5dc54f6d3129624a3fa306eb218fd54dd17bae5f
Carlos Dev <carlosmpr.ing.sistemas@gmail.com>
2026-06-06 12:49:11 -0400

    Modificaion y adicion de nueva ruta protegida dashboard

--- blueprintapp/core/routes.py
+++ blueprintapp/core/routes.py
@@ -1,9 +1,12 @@
-from flask import request, render_template, redirect, url_for, Blueprint
-
-from blueprintapp.app import db
-
-bp_core = Blueprint('bp_core',__name__,template_folder='templates')
+from flask import render_template
+from flask_login import login_required
+from blueprintapp.core import bp_core
 
 @bp_core.route("/")
 def index():
     return render_template('core/index.html')
+
+@bp_core.route("/dashboard")
+@login_required
+def dashboard():
+    return render_template("core/dashboard.html")
